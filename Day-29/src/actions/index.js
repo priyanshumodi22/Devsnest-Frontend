61f0db408b2e56fa01348fb583f67907225ecaf5
priyanshumodi22 <priyanshumodi2001@gmail.com>
2021-08-12 22:15:24 +0530

    test

---
+++ Day-29/src/actions/index.js
@@ -0,0 +1,9 @@
+const updatePlace = (place) => {
+  return { type: "UPDATE_PLACE", payload: place };
+};
+
+const addLocation = (location) => {
+  return { type: "ADD_LOCATION", payload: location };
+};
+
+export { updatePlace, addLocation };
